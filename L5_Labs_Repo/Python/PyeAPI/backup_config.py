fab8642726e848f575549d68ab5c6d80830813f0
mrmozaffari <ba.mozaffari@gmail.com>
2025-10-14 05:20:12 +0000

    Save

---
+++ L5_Labs_Repo/Python/PyeAPI/backup_config.py
@@ -0,0 +1,26 @@
+# %%
+import pyeapi
+import os
+
+pyeapi.load_config('./eapi.conf')
+#connections = pyeapi.config_for('leaf1')
+
+# %%
+directory = "configs"
+exists = os.path.exists(directory)
+if not exists:
+    os.makedirs(directory)
+
+# %%
+switches = ['leaf1', 'leaf2', 'leaf3', 'leaf4', 'spine1', 'spine2', 'spine3', 'spine4', 'borderleaf1', 'borderleaf2']
+
+# %%
+for switch in switches:
+    connect = pyeapi.connect_to(switch)
+    running_config = connect.get_config(as_string='True')
+    path = directory+'/'+switch+'.cfg'
+    with open(path, 'w') as f:
+        f.write(running_config)
+
+
+
